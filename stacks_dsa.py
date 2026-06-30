467c83ad06cf28a730e32b016f0fa2dc7e125ca1
SyedSaif07 <syed.sky07@gmail.com>
2026-06-30 21:50:06 +0530

    advanced dsa problems

--- stacks_dsa.py
+++ stacks_dsa.py
@@ -226,4 +226,24 @@ class LongestValidParentheses:
         return max_len
 
 
-print(LongestValidParentheses().longestValidParentheses("(())"))
+# print(LongestValidParentheses().longestValidParentheses("(())"))
+
+class DecodeString:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        for i in range(len(s)):
+            if s[i] != "]":
+                stack.append(s[i])
+            else:
+                substr = ""
+                while stack[-1] != "[":
+                    substr = stack.pop() + substr
+                stack.pop()
+                k = ""
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k
+                stack.append(int(k) * substr)
+        return "".join(stack)
+
+
+print(DecodeString().decodeString(s="3[a2[c]]"))
