e099e426aa384ccda24462fa2b386a5a6c9aac2f
SyedSaif07 <syed.sky07@gmail.com>
2026-07-29 15:01:56 +0530

    unique paths problem

--- dynamic_programming_dsa.py
+++ dynamic_programming_dsa.py
@@ -396,3 +396,28 @@ class DecodeWays:
 
 
 # print(DecodeWays().numDecodings(s="226"))
+
+
+class UniquePaths:
+    """
+    There is a robot on an m x n grid.
+    The robot is initially located at the top-left corner (i.e., grid[0][0]).
+    The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]).
+    The robot can only move either down or right at any point in time.
+
+    Given the two integers m and n, return the number of possible unique paths
+    that the robot can take to reach the bottom-right corner.
+    """
+
+    def uniquePaths(self, m: int, n: int) -> int:
+        aboveRow = [1] * n
+
+        for _ in range(m - 1):
+            currentRow = [1] * n
+            for i in range(1, n):
+                currentRow[i] = currentRow[i - 1] + aboveRow[i]
+            aboveRow = currentRow
+        return aboveRow[-1]
+
+
+print(UniquePaths().uniquePaths(23, 12))
